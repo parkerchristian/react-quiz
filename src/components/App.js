37489ba160662d379678ce25f2cb4ddd3f32c1f1
Parker Helland <hellandp@gmail.com>
2019-05-20 09:39:29 -0700

    initial

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import NameTag from './container/NameTag';
+
+
+export default function App() {
+  return <NameTag />;
+}
---
+++ src/components/container/NameTag.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NameTag() {
+  return <h1>sup</h1>;
+}
